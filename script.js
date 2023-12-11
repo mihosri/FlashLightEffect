a04988067d019eb7171f92b3a3b8c0d6b53aceb2
mihosri <srineethi.gurumurthigirahhalakshmi@edu.sait.ca>
2023-12-10 23:05:41 -0700

    Updated Script file
    added touchDevice function

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 let mouseX = 0;
 let mouseY = 0;
+let flashlight = document.getElementById("flashlight");
+
+const isTouchDevice = () => {
+    try{
+        document.creatreEvent("TouchEvent");
+        return true;
+    }
+    catch(e)
+    {
+        return false;
+    }
+}
